1d31e4c32f362c61486266cf070dafb8a645e5c3
drrefactor <drrefactor@gmail.com>
2024-04-10 08:06:50 +0200

    Add power bot

---
+++ bots/power.js
@@ -0,0 +1,124 @@
+const room = HBInit({
+  roomName: "Jeze",
+  playerName: "",
+  noPlayer: true,
+  public: false,
+  maxPlayers: 12,
+});
+
+const ballRadius = 10;
+const playerRadius = 15;
+const triggerDistance = ballRadius + playerRadius + 75;
+const triggerTouchTime = 80;
+
+const TEAM = {
+  RED: 1,
+  BLUE: 2,
+  SPECTATOR: 3,
+};
+
+const powerShotRatio = {
+  [TEAM.RED]: 1.8,
+  [TEAM.BLUE]: 1.8
+};
+
+let playerTouchTime = {};
+
+function pointDistance(p1, p2) {
+  const d1 = p1.x - p2.x;
+  const d2 = p1.y - p2.y;
+  return Math.sqrt(d1 * d1 + d2 * d2);
+}
+
+function saveTouchTime() {
+  room.getPlayerList().forEach((player) => {
+    const distanceToBall = pointDistance(
+      room.getPlayerDiscProperties(player.id),
+      room.getDiscProperties(0)
+    );
+    const touches = distanceToBall < triggerDistance;
+
+    if (touches) {
+      playerTouchTime[player.id] = playerTouchTime[player.id] || 0;
+      playerTouchTime[player.id]++;
+    } else {
+      playerTouchTime[player.id] = 0;
+    }
+  });
+}
+
+function updateAvatars() {
+  Object.entries(playerTouchTime).forEach(([playerId, playerTouchTime]) => {
+    if (playerTouchTime >= triggerTouchTime) {
+      room.setPlayerAvatar(playerId, "P");
+    } else if (playerTouchTime > 0) {
+      const a = Math.round((10 * playerTouchTime) / triggerTouchTime);
+      room.setPlayerAvatar(playerId, "." + a);
+    } else {
+      room.setPlayerAvatar(playerId, null);
+    }
+  });
+}
+
+function resetTouchTimes() {
+  playerTouchTime = {};
+  updateAvatars();
+}
+
+room.onPlayerBallKick = function (player) {
+  const powerActive = playerTouchTime[player.id] >= triggerTouchTime;
+  if (powerActive) {
+    const ps = powerShotRatio[player.team];
+    room.setDiscProperties(0, {
+      xspeed: ps * room.getDiscProperties(0).xspeed,
+      yspeed: ps * room.getDiscProperties(0).yspeed,
+    });
+    resetTouchTimes();
+  }
+};
+
+room.onGameStop = function (byPlayer) {
+  playerTouchTime = {};
+};
+
+room.onGameTick = function () {
+  if (room.getPlayerList().filter((p) => p.team != 0).length > 0) {
+    saveTouchTime();
+  }
+  updateAvatars();
+};
+
+room.onPlayerChat = function (player, message) {
+  console.log(player, `message: "${message}"`);
+  if (message.toLowerCase().startsWith("!reset")) {
+    room.setDiscProperties(0, {
+      x: 0,
+      y: 0,
+    });
+  }
+  if (message.toLowerCase().startsWith("!power")) {
+    const [_, teamIdString, powerString] = message.split(',');
+    const power = Number.parseFloat(powerString);
+    const team = Number.parseInt(teamIdString);
+    if (!Number.isNaN(power) && !Number.isNaN(team)) {
+      powerShotRatio[team] = power;
+    }
+  }
+};
+
+// room.getPlayerList()
+// [] {
+//   "name": "n",
+//   "team": 1,
+//   "id": 1,
+//   "admin": true,
+//   "position": null
+// }
+
+let r = true;
+room.onPlayerJoin = function (player) {
+  room.setPlayerAdmin(player.id, true);
+  room.setPlayerTeam(player.id, r ? 1 : 2);
+  console.log(player);
+  r = !r;
+};
